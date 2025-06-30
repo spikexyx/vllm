f421a2b8ac76534dafd34bdf86f3e128c95b49a4
岑潇 <xuyixiao.xyx@alibaba-inc.com>
2025-06-30 19:06:14 +0800

    Add patch to other model runner types.

--- vllm_weight_hook_patch_core.py
+++ vllm_weight_hook_patch_core.py
@@ -182,11 +182,12 @@ def _patched_clear_old_weight_data(self):
         return
 
 # ===================================================================
-# Monkey patch the ModelRunner class methods
+# Monkey patch the ModelRunner classes load_model method
 
 def apply_vllm_model_runner_patches():
     print(f"[PATCH] Applying model runner patches in process {os.getpid()}...")
     try:
+        # ==============================   v0   ===============================
         # Patch v0 GPUModelRunnerBase load_model
         from vllm.worker.model_runner import GPUModelRunnerBase
 
@@ -199,7 +200,7 @@ def apply_vllm_model_runner_patches():
         GPUModelRunnerBase._clear_old_weight_data = _patched_clear_old_weight_data
 
         if not hasattr(GPUModelRunnerBase, '_original_load_model'):
-            print("[VLLM_PATCH_CORE] Start to patching GPUModelRunnerBase.load_model to handle weight metadata loading")
+            # print("[VLLM_PATCH_CORE] Start to patching GPUModelRunnerBase.load_model to handle weight metadata loading")
             GPUModelRunnerBase._original_load_model = GPUModelRunnerBase.load_model
             def patched_load_model(self):
                 print("[VLLM_PATCH_CORE] Patched GPUModelRunnerBase.load_model to handle weight metadata loading")
@@ -208,6 +209,154 @@ def apply_vllm_model_runner_patches():
                 self._register_weight_hooks()
             GPUModelRunnerBase.load_model = patched_load_model
 
+        # Patch v0 hpu_model_runner load_model
+        from vllm.worker.hpu_model_runner import HPUModelRunnerBase
+
+        HPUModelRunnerBase._acquire_weight_lock = _patched_acquire_weight_lock
+        HPUModelRunnerBase._release_weight_lock = _patched_release_weight_lock
+        HPUModelRunnerBase._register_weight_hooks = _patched_register_weight_hooks
+        HPUModelRunnerBase._save_weight_meta = _patched_save_weight_meta
+        HPUModelRunnerBase._save_total_weight_meta = _patched_save_total_weight_meta
+        HPUModelRunnerBase._calculate_device_weight_sizes = _patched_calculate_device_weight_sizes
+        HPUModelRunnerBase._clear_old_weight_data = _patched_clear_old_weight_data
+
+        if not hasattr(HPUModelRunnerBase, '_original_load_model'):
+            # print("[VLLM_PATCH_CORE] Start to patching HPUModelRunnerBase.load_model to handle weight metadata loading")
+            HPUModelRunnerBase._original_load_model = HPUModelRunnerBase.load_model
+            def patched_load_model(self):
+                print("[VLLM_PATCH_CORE] Patched HPUModelRunnerBase.load_model to handle weight metadata loading")
+                self._original_load_model()
+                # Register hooks after model is loaded
+                self._register_weight_hooks()
+            HPUModelRunnerBase.load_model = patched_load_model
+
+        # Patch v0 multi_step_neuron_model_runner load_model
+        from vllm.worker.multi_step_neuron_model_runner import MultiStepNeuronModelRunner
+
+        MultiStepNeuronModelRunner._acquire_weight_lock = _patched_acquire_weight_lock
+        MultiStepNeuronModelRunner._release_weight_lock = _patched_release_weight_lock
+        MultiStepNeuronModelRunner._register_weight_hooks = _patched_register_weight_hooks
+        MultiStepNeuronModelRunner._save_weight_meta = _patched_save_weight_meta
+        MultiStepNeuronModelRunner._save_total_weight_meta = _patched_save_total_weight_meta
+        MultiStepNeuronModelRunner._calculate_device_weight_sizes = _patched_calculate_device_weight_sizes
+        MultiStepNeuronModelRunner._clear_old_weight_data = _patched_clear_old_weight_data
+
+        if not hasattr(MultiStepNeuronModelRunner, '_original_load_model'):
+            # print("[VLLM_PATCH_CORE] Start to patching MultiStepNeuronModelRunner.load_model to handle weight metadata loading")
+            MultiStepNeuronModelRunner._original_load_model = MultiStepNeuronModelRunner.load_model
+            def patched_load_model(self):
+                print("[VLLM_PATCH_CORE] Patched MultiStepNeuronModelRunner.load_model to handle weight metadata loading")
+                self._original_load_model()
+                # Register hooks after model is loaded
+                self._register_weight_hooks()
+            MultiStepNeuronModelRunner.load_model = patched_load_model
+
+        # Patch v0 multi_step_neuronx_distributed_model_runner load_model
+        from vllm.worker.multi_step_neuronx_distributed_model_runner import MultiStepNeuronxDistributedModelRunner
+
+        MultiStepNeuronxDistributedModelRunner._acquire_weight_lock = _patched_acquire_weight_lock
+        MultiStepNeuronxDistributedModelRunner._release_weight_lock = _patched_release_weight_lock
+        MultiStepNeuronxDistributedModelRunner._register_weight_hooks = _patched_register_weight_hooks
+        MultiStepNeuronxDistributedModelRunner._save_weight_meta = _patched_save_weight_meta
+        MultiStepNeuronxDistributedModelRunner._save_total_weight_meta = _patched_save_total_weight_meta
+        MultiStepNeuronxDistributedModelRunner._calculate_device_weight_sizes = _patched_calculate_device_weight_sizes
+        MultiStepNeuronxDistributedModelRunner._clear_old_weight_data = _patched_clear_old_weight_data
+
+        if not hasattr(MultiStepNeuronxDistributedModelRunner, '_original_load_model'):
+            # print("[VLLM_PATCH_CORE] Start to patching MultiStepNeuronxDistributedModelRunner.load_model to handle weight metadata loading")
+            MultiStepNeuronxDistributedModelRunner._original_load_model = MultiStepNeuronxDistributedModelRunner.load_model
+            def patched_load_model(self):
+                print("[VLLM_PATCH_CORE] Patched MultiStepNeuronxDistributedModelRunner.load_model to handle weight metadata loading")
+                self._original_load_model()
+                # Register hooks after model is loaded
+                self._register_weight_hooks()
+            MultiStepNeuronxDistributedModelRunner.load_model = patched_load_model
+
+        # Patch v0 neuron_model_runner load_model
+        from vllm.worker.neuron_model_runner import NeuronModelRunner
+
+        NeuronModelRunner._acquire_weight_lock = _patched_acquire_weight_lock
+        NeuronModelRunner._release_weight_lock = _patched_release_weight_lock
+        NeuronModelRunner._register_weight_hooks = _patched_register_weight_hooks
+        NeuronModelRunner._save_weight_meta = _patched_save_weight_meta
+        NeuronModelRunner._save_total_weight_meta = _patched_save_total_weight_meta
+        NeuronModelRunner._calculate_device_weight_sizes = _patched_calculate_device_weight_sizes
+        NeuronModelRunner._clear_old_weight_data = _patched_clear_old_weight_data   
+
+        if not hasattr(NeuronModelRunner, '_original_load_model'):
+            # print("[VLLM_PATCH_CORE] Start to patching NeuronModelRunner.load_model to handle weight metadata loading")
+            NeuronModelRunner._original_load_model = NeuronModelRunner.load_model
+            def patched_load_model(self):
+                print("[VLLM_PATCH_CORE] Patched NeuronModelRunner.load_model to handle weight metadata loading")
+                self._original_load_model()
+                # Register hooks after model is loaded
+                self._register_weight_hooks()
+            NeuronModelRunner.load_model = patched_load_model
+
+        # Patch v0 neuronx_distributed_model_runner load_model
+        from vllm.worker.neuronx_distributed_model_runner import NeuronxDistributedModelRunner
+
+        NeuronxDistributedModelRunner._acquire_weight_lock = _patched_acquire_weight_lock
+        NeuronxDistributedModelRunner._release_weight_lock = _patched_release_weight_lock
+        NeuronxDistributedModelRunner._register_weight_hooks = _patched_register_weight_hooks
+        NeuronxDistributedModelRunner._save_weight_meta = _patched_save_weight_meta
+        NeuronxDistributedModelRunner._save_total_weight_meta = _patched_save_total_weight_meta
+        NeuronxDistributedModelRunner._calculate_device_weight_sizes = _patched_calculate_device_weight_sizes
+        NeuronxDistributedModelRunner._clear_old_weight_data = _patched_clear_old_weight_data
+
+        if not hasattr(NeuronxDistributedModelRunner, '_original_load_model'):
+            # print("[VLLM_PATCH_CORE] Start to patching NeuronxDistributedModelRunner.load_model to handle weight metadata loading")
+            NeuronxDistributedModelRunner._original_load_model = NeuronxDistributedModelRunner.load_model
+            def patched_load_model(self):
+                print("[VLLM_PATCH_CORE] Patched NeuronxDistributedModelRunner.load_model to handle weight metadata loading")
+                self._original_load_model()
+                # Register hooks after model is loaded
+                self._register_weight_hooks()
+            NeuronxDistributedModelRunner.load_model = patched_load_model
+
+        # Patch v0 tpu_model_runner load_model
+        from vllm.worker.tpu_model_runner import TPUModelRunner
+
+        TPUModelRunner._acquire_weight_lock = _patched_acquire_weight_lock
+        TPUModelRunner._release_weight_lock = _patched_release_weight_lock
+        TPUModelRunner._register_weight_hooks = _patched_register_weight_hooks
+        TPUModelRunner._save_weight_meta = _patched_save_weight_meta
+        TPUModelRunner._save_total_weight_meta = _patched_save_total_weight_meta
+        TPUModelRunner._calculate_device_weight_sizes = _patched_calculate_device_weight_sizes
+        TPUModelRunner._clear_old_weight_data = _patched_clear_old_weight_data
+
+        if not hasattr(TPUModelRunner, '_original_load_model'):
+            # print("[VLLM_PATCH_CORE] Start to patching TPUModelRunner.load_model to handle weight metadata loading")
+            TPUModelRunner._original_load_model = TPUModelRunner.load_model
+            def patched_load_model(self):
+                print("[VLLM_PATCH_CORE] Patched TPUModelRunner.load_model to handle weight metadata loading")
+                self._original_load_model()
+                # Register hooks after model is loaded
+                self._register_weight_hooks()
+            TPUModelRunner.load_model = patched_load_model
+
+        # Patch v0 xpu_model_runner load_model
+        from vllm.worker.xpu_model_runner import XPUModelRunner
+
+        XPUModelRunner._acquire_weight_lock = _patched_acquire_weight_lock
+        XPUModelRunner._release_weight_lock = _patched_release_weight_lock
+        XPUModelRunner._register_weight_hooks = _patched_register_weight_hooks
+        XPUModelRunner._save_weight_meta = _patched_save_weight_meta
+        XPUModelRunner._save_total_weight_meta = _patched_save_total_weight_meta
+        XPUModelRunner._calculate_device_weight_sizes = _patched_calculate_device_weight_sizes
+        XPUModelRunner._clear_old_weight_data = _patched_clear_old_weight_data
+
+        if not hasattr(XPUModelRunner, '_original_load_model'):
+            # print("[VLLM_PATCH_CORE] Start to patching XPUModelRunner.load_model to handle weight metadata loading")
+            XPUModelRunner._original_load_model = XPUModelRunner.load_model
+            def patched_load_model(self):
+                print("[VLLM_PATCH_CORE] Patched XPUModelRunner.load_model to handle weight metadata loading")
+                self._original_load_model()
+                # Register hooks after model is loaded
+                self._register_weight_hooks()
+            XPUModelRunner.load_model = patched_load_model
+
+        # ==============================   v1   ===============================
         # Patch v1 GPUModelRunner load_model
         from vllm.v1.worker.gpu_model_runner import GPUModelRunner
 
@@ -220,7 +369,7 @@ def apply_vllm_model_runner_patches():
         GPUModelRunner._clear_old_weight_data = _patched_clear_old_weight_data
 
         if not hasattr(GPUModelRunner, '_original_load_model'):
-            print("[VLLM_PATCH_CORE] Start to patching GPUModelRunner.load_model to handle weight metadata loading")
+            # print("[VLLM_PATCH_CORE] Start to patching GPUModelRunner.load_model to handle weight metadata loading")
             GPUModelRunner._original_load_model = GPUModelRunner.load_model
             def patched_load_model(self):
                 print("[VLLM_PATCH_CORE] Patched GPUModelRunner.load_model to handle weight metadata loading")
@@ -228,6 +377,29 @@ def apply_vllm_model_runner_patches():
                 # Register hooks after model is loaded
                 self._register_weight_hooks()
             GPUModelRunner.load_model = patched_load_model
+
+        # Patch v1 tpu_model_runner load_model
+        from vllm.v1.worker.tpu_model_runner import TPUModelRunner
+
+        TPUModelRunner._acquire_weight_lock = _patched_acquire_weight_lock
+        TPUModelRunner._release_weight_lock = _patched_release_weight_lock
+        TPUModelRunner._register_weight_hooks = _patched_register_weight_hooks
+        TPUModelRunner._save_weight_meta = _patched_save_weight_meta
+        TPUModelRunner._save_total_weight_meta = _patched_save_total_weight_meta
+        TPUModelRunner._calculate_device_weight_sizes = _patched_calculate_device_weight_sizes
+        TPUModelRunner._clear_old_weight_data = _patched_clear_old_weight_data
+
+        if not hasattr(TPUModelRunner, '_original_load_model'):
+            # print("[VLLM_PATCH_CORE] Start to patching TPUModelRunner.load_model to handle weight metadata loading")
+            TPUModelRunner._original_load_model = TPUModelRunner.load_model
+            def patched_load_model(self):
+                print("[VLLM_PATCH_CORE] Patched TPUModelRunner.load_model to handle weight metadata loading")
+                self._original_load_model()
+                # Register hooks after model is loaded
+                self._register_weight_hooks()
+            TPUModelRunner.load_model = patched_load_model
+
+        # ===============================   end   ===============================
             
     except Exception as e:
         print(f"[VLLM_PATCH_CORE] Failed to apply GPUModelRunnerBase patches: {e}")
